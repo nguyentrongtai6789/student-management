a535bd1dc3ff9cb6ebf2b88ff2bf289a137fd92c
Trung Do CodeGym <trung96ecvn@gmail.com>
2023-09-25 11:27:38 +0700

    bổ xung thêm 3 controller

--- src/main/java/com/example/studentmanagement/controller/DetailStudentAndSubjectController.java
+++ src/main/java/com/example/studentmanagement/controller/DetailStudentAndSubjectController.java
@@ -1,2 +1,11 @@
-package com.example.studentmanagement.controller;public class DetailStudentAndSubjectController {
+package com.example.studentmanagement.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/detail")
+public class DetailStudentAndSubjectController {
 }
--- src/main/java/com/example/studentmanagement/controller/SubjectController.java
+++ src/main/java/com/example/studentmanagement/controller/SubjectController.java
@@ -1,2 +1,11 @@
-package com.example.studentmanagement.controller;public class SubjectController {
+package com.example.studentmanagement.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/subject")
+public class SubjectController {
 }
